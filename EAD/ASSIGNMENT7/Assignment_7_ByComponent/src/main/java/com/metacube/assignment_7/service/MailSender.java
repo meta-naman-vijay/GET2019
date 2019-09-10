32f31726c9710c63e89d64dfacdabd811c78d5fc
meta-naman-vijay <naman.vijay@metacube.com>
2019-09-10 15:51:41 +0530

    EAD/ASSIGNMENT7

---
+++ EAD/ASSIGNMENT7/Assignment_7_ByComponent/src/main/java/com/metacube/assignment_7/service/MailSender.java
@@ -0,0 +1,15 @@
+package com.metacube.assignment_7.service;
+
+/**
+ * This is mail sender Interface
+ * @author Naman
+ *
+ */
+public interface MailSender {
+
+	/**
+	 * This method send mail
+	 * @return true if mail send else false
+	 */
+	public boolean sendMail();
+}
---
+++ EAD/ASSIGNMENT7/Assignment_7_ByConfig/src/main/java/com/metacube/assignment_7/service/MailSender.java
@@ -0,0 +1,15 @@
+package com.metacube.assignment_7.service;
+
+/**
+ * This is mail sender Interface
+ * @author NAman
+ *
+ */
+public interface MailSender {
+
+	/**
+	 * This method send mail
+	 * @return true if mail send else false
+	 */
+	public boolean sendMail();
+}
